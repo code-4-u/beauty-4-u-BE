d5831f620c87226a558d62bc58b2c953458a22b4
seolbin01 <se01bin@suwon.ac.kr>
2024-12-03 00:32:35 +0900

    feat: 커스텀 UserDetailService 구현 #5

---
+++ src/main/java/com/beauty4u/backend/security/util/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.beauty4u.backend.security.util;
+
+import com.beauty4u.backend.user.command.domain.aggregate.UserInfo;
+import com.beauty4u.backend.user.command.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+
+        UserInfo loginUser = userRepository.findByUserCode(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException(loginId));
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + loginUser.getUserRole().getUserRoleName()));
+
+        return new User(
+                loginUser.getUserCode(),
+                loginUser.getUserPassword(),
+                grantedAuthorities
+        );
+    }
+}
