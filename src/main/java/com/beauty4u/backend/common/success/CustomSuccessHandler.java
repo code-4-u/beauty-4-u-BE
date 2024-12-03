208aca6303f3ce9aa7b1e6ba9a8c6f96b753a316
seolbin01 <se01bin@suwon.ac.kr>
2024-12-03 15:14:49 +0900

    feat: 공통 응답 유틸 구현 #11

--- src/main/java/com/beauty4u/backend/common/success/CustomSuccessHandler.java
+++ src/main/java/com/beauty4u/backend/common/success/CustomSuccessHandler.java
@@ -1,27 +1,26 @@
 package com.beauty4u.backend.common.success;
 
+import com.beauty4u.backend.common.response.ApiResponse;
+import com.beauty4u.backend.common.response.ResponseUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomSuccessHandler {
 
-    public <T> ResponseEntity<CustomSuccess<T>> createSuccessResponse(SuccessCode successCode, T data) {
-        return ResponseEntity
-                .status(successCode.getHttpStatus())
-                .body(new CustomSuccess<>(
-                        successCode.name(),
-                        successCode.getMessage(),
-                        data
-                ));
+    public <T> ResponseEntity<ApiResponse<T>> createSuccessResponse(SuccessCode successCode, T data) {
+        return ResponseUtil.successResponse(
+                successCode.getMessage(),
+                data,
+                successCode.getHttpStatus()
+        );
     }
 
-    public ResponseEntity<CustomSuccess<Void>> createSuccessResponse(SuccessCode successCode) {
-        return ResponseEntity
-                .status(successCode.getHttpStatus())
-                .body(new CustomSuccess<>(
-                        successCode.name(),
-                        successCode.getMessage()
-                ));
+    public ResponseEntity<ApiResponse<Void>> createSuccessResponse(SuccessCode successCode) {
+        return ResponseUtil.successResponse(
+                successCode.getMessage(),
+                null,
+                successCode.getHttpStatus()
+        );
     }
 }
