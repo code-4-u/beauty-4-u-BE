1bd9bcb0fd0e2adb1c815a7e3ab5dc395ef5ff7d
leebackcoding <lcy2825@gmail.com>
2024-12-04 16:25:36 +0900

    feat: SSP-67 고객 목록 조회 dto 추가

---
+++ src/main/java/com/beauty4u/backend/customer/query/dto/CustomerListResDTO.java
@@ -0,0 +1,22 @@
+package com.beauty4u.backend.customer.query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerListResDTO {
+
+    private String customerCode;
+    private String customerName;
+    private CustomerGrade customerGrade;
+    private Long customerAge;
+    private LocalDateTime customerLastOrderDate;
+    private LocalDateTime customerCreatedDate;
+}
