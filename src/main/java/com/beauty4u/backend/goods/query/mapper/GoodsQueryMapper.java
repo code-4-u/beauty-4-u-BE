7da27cdfef3a374472e16418db742c14ac52a056
SaeRyung <sr1094@naver.com>
2024-12-04 16:32:28 +0900

    feat: GoodsQueryMapper 상위카테고리 클릭 시 포함하는 하위 카테고리, 상위 내 상품 목록 조회 구현 완료

--- src/main/java/com/beauty4u/backend/goods/query/mapper/GoodsQueryMapper.java
+++ src/main/java/com/beauty4u/backend/goods/query/mapper/GoodsQueryMapper.java
@@ -2,6 +2,7 @@ package com.beauty4u.backend.goods.query.mapper;
 
 import com.beauty4u.backend.goods.query.dto.BrandQueryDTO;
 import com.beauty4u.backend.goods.query.dto.GoodsQueryDTO;
+import com.beauty4u.backend.goods.query.dto.SubCategoryDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -17,4 +18,10 @@ public interface GoodsQueryMapper {
             @Param("brandCode") String brandCode,
             @Param("goodsName") String goodsName
     );
+
+    // 상위 카테고리에 내에 있는 하위 카테고리 조회
+    List<SubCategoryDTO> findSubCategory(String topCategoryCode);
+
+    // 상위 카테고리에 해당하는 상품 메인화면에 전체 조회
+    List<GoodsQueryDTO> findTopCategoryGoods(String topCategoryCode);
 }
