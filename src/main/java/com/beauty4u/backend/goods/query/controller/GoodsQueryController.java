4371816888086438a6b492c3422e7045fabe39e2
SaeRyung <sr1094@naver.com>
2024-12-04 09:08:59 +0900

    feat: GoodsQueryController 상품브랜드 조회 구현

--- src/main/java/com/beauty4u/backend/goods/query/controller/GoodsQueryController.java
+++ src/main/java/com/beauty4u/backend/goods/query/controller/GoodsQueryController.java
@@ -1,5 +1,7 @@
 package com.beauty4u.backend.goods.query.controller;
 
+import com.beauty4u.backend.common.success.CustomSuccessHandler;
+import com.beauty4u.backend.common.success.SuccessCode;
 import com.beauty4u.backend.goods.query.dto.GoodsQueryDTO;
 import com.beauty4u.backend.goods.query.service.GoodsQueryService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -7,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +22,21 @@ import java.util.List;
 public class GoodsQueryController {
 
     private final GoodsQueryService goodsQueryService;
+    private final CustomSuccessHandler customSuccessHandler;
 
     @GetMapping("/list")
-    @Operation(summary = "상품 목록 조회", description = "상품 목록을 조회한다.")
+    @Operation(summary = "상품 목록 조회", description = "전체 상품 목록을 조회한다.")
     public ResponseEntity<List<GoodsQueryDTO>> findAllGoods(){
         List<GoodsQueryDTO> goodsDTOList = goodsQueryService.findAllGoods();
 
         return ResponseEntity.ok(goodsDTOList);
+    }
+
+    @GetMapping("/brand/{brandCode}")
+    @Operation(summary = "브랜드 별 상품 조회", description = "브랜드 별 상품을 조회한다.")
+    public ResponseEntity<List<GoodsQueryDTO>> findAllBrand(@PathVariable String brandCode){
+        List<GoodsQueryDTO> goodsBrandList = goodsQueryService.findAllBrand(brandCode);
+
+        return ResponseEntity.ok(goodsBrandList);
     }
 }
