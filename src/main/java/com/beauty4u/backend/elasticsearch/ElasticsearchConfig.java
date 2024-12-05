f867e065e3ca86a7c24b642fa563b385f56b37ce
SaeRyung <sr1094@naver.com>
2024-12-05 16:57:51 +0900

    feat: 엘라스틱 서치 인덱스 설정값 적용

--- src/main/java/com/beauty4u/backend/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/beauty4u/backend/elasticsearch/ElasticsearchConfig.java
@@ -1,11 +1,13 @@
-package com.beauty4u.backend.elasticesearch;
+package com.beauty4u.backend.elasticsearch;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@EnableElasticsearchRepositories(basePackages = "com.beauty4u.backend.goods.query.elasticsearch.repository")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.elasticsearch.uris}")
     private String elasticsearchUris;
@@ -16,11 +18,16 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
     @Value("${spring.elasticsearch.password}")
     private String elasticsearchPassword;
 
+    @Value("${spring.elasticsearch.index.name}")
+    private String goodsName;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo(elasticsearchUris.replace("http://",""))
                 .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
+                .withSocketTimeout(30000)
+                .withConnectTimeout(30000)
                 .build();
     }
 }
