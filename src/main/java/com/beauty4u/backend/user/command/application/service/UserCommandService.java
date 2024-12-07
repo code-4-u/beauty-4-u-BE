0378642e45c3ec2a279646d0f30199672b45a209
seolbin01 <se01bin@suwon.ac.kr>
2024-12-07 21:54:53 +0900

    feat: 비밀번호 수정 service 메서드 구현 #54

--- src/main/java/com/beauty4u/backend/user/command/application/service/UserCommandService.java
+++ src/main/java/com/beauty4u/backend/user/command/application/service/UserCommandService.java
@@ -4,11 +4,8 @@ import com.beauty4u.backend.common.exception.CustomException;
 import com.beauty4u.backend.common.exception.ErrorCode;
 import com.beauty4u.backend.common.util.MailUtil;
 import com.beauty4u.backend.config.redis.RedisService;
-import com.beauty4u.backend.user.command.application.dto.CreateUserRequest;
-import com.beauty4u.backend.user.command.application.dto.LoginUserReqDTO;
-import com.beauty4u.backend.user.command.application.dto.ResetUserPasswordReqDTO;
+import com.beauty4u.backend.user.command.application.dto.*;
 import com.beauty4u.backend.user.command.domain.service.UserDomainService;
-import com.beauty4u.backend.user.command.application.dto.FindUserCodeReqDTO;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -153,5 +150,13 @@ public class UserCommandService {
         }
 
         return new String(passwordArray);
+    }
+
+    @Transactional
+    public void updateUserPassword(String loginUserCode, UpdateUserPasswordReqDTO updateUserPasswordReqDTO) {
+
+        String newPassword = updateUserPasswordReqDTO.getUserPassword();
+
+        userDomainService.updatePassword(loginUserCode, newPassword);
     }
 }
