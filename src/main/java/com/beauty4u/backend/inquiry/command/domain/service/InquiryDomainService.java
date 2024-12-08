f2242e9f86bb0728d5d9b54ded3d754c08c2e224
seolbin01 <se01bin@suwon.ac.kr>
2024-12-08 23:00:15 +0900

    feat: QnA 질문 수정 domainService 메서드 구현 #74

--- src/main/java/com/beauty4u/backend/inquiry/command/domain/service/InquiryDomainService.java
+++ src/main/java/com/beauty4u/backend/inquiry/command/domain/service/InquiryDomainService.java
@@ -2,7 +2,7 @@ package com.beauty4u.backend.inquiry.command.domain.service;
 
 import com.beauty4u.backend.common.exception.CustomException;
 import com.beauty4u.backend.common.exception.ErrorCode;
-import com.beauty4u.backend.inquiry.command.application.dto.CreateQnaReqDTO;
+import com.beauty4u.backend.inquiry.command.application.dto.QnaReqDTO;
 import com.beauty4u.backend.inquiry.command.domain.aggregate.Inquiry;
 import com.beauty4u.backend.inquiry.command.domain.repository.InquiryRepository;
 import com.beauty4u.backend.user.command.domain.aggregate.UserInfo;
@@ -19,9 +19,9 @@ public class InquiryDomainService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
-    public void saveQna(String loginUserCode, CreateQnaReqDTO createQnaReqDTO) {
+    public void saveQna(String loginUserCode, QnaReqDTO qnaReqDTO) {
 
-        Inquiry inquiry = modelMapper.map(createQnaReqDTO, Inquiry.class);
+        Inquiry inquiry = modelMapper.map(qnaReqDTO, Inquiry.class);
 
         UserInfo user = userRepository.findByUserCode(loginUserCode)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
@@ -33,5 +33,17 @@ public class InquiryDomainService {
         } catch (Exception e) {
             throw new CustomException(ErrorCode.NOT_SAVED_INQUIRY);
         }
+    }
+
+    public void updateQna(Long inquiryId, QnaReqDTO qnaReqDTO) {
+
+        String title = qnaReqDTO.getInquiryTitle();
+        String content = qnaReqDTO.getInquiryContent();
+        Character secretYn = qnaReqDTO.getInquirySecretYn();
+
+        Inquiry inquiry = inquiryRepository.findById(inquiryId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_INQUIRY));
+
+        inquiry.modifyInquiry(title, content, secretYn);
     }
 }
