059145435c014c8d03303c87537a6541e25c6ded
seolbin01 <se01bin@suwon.ac.kr>
2024-12-08 15:29:35 +0900

    feat: 공지사항 등록 요청 dto 구현 #60

---
+++ src/main/java/com/beauty4u/backend/inform/command/application/dto/CreateInformReqDTO.java
@@ -0,0 +1,16 @@
+package com.beauty4u.backend.inform.command.application.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateInformReqDTO {
+
+    @NotNull(message = "제목은 필수 입력값입니다.")
+    private String informTitle;
+
+    @NotNull(message = "내용은 필수 입력값입니다.")
+    private String informContent;
+}
