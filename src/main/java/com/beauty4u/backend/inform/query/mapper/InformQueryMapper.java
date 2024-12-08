94c7ba0515426e4478982f83f93085dcca24c090
seolbin01 <se01bin@suwon.ac.kr>
2024-12-08 20:40:02 +0900

    feat: 공지사항 상세 조회 mapper 메서드 구현 #70

--- src/main/java/com/beauty4u/backend/inform/query/mapper/InformQueryMapper.java
+++ src/main/java/com/beauty4u/backend/inform/query/mapper/InformQueryMapper.java
@@ -1,5 +1,6 @@
 package com.beauty4u.backend.inform.query.mapper;
 
+import com.beauty4u.backend.inform.query.dto.InformDetailResDTO;
 import com.beauty4u.backend.inform.query.dto.InformListResDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -12,4 +13,6 @@ public interface InformQueryMapper {
     List<InformListResDTO> findInformList(
             @Param("offset") Long offset,
             @Param("count") Long count);
+
+    InformDetailResDTO findInformDetail(@Param("informId") Long informId);
 }
