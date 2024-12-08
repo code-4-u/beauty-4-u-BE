c80ab58f8dfa170d4790f0e1959c3026194b5dbb
SaeRyung <sr1094@naver.com>
2024-12-09 03:43:37 +0900

    feat: TemplateCommandService 템플릿 수정 코드 추가

--- src/main/java/com/beauty4u/backend/template/command/application/service/TemplateCommandService.java
+++ src/main/java/com/beauty4u/backend/template/command/application/service/TemplateCommandService.java
@@ -1,6 +1,7 @@
 package com.beauty4u.backend.template.command.application.service;
 
 import com.beauty4u.backend.template.command.application.dto.CreateTemplateReqDTO;
+import com.beauty4u.backend.template.command.application.dto.UpdateTemplateReqDTO;
 import com.beauty4u.backend.template.command.domain.service.TemplateDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,9 @@ public class TemplateCommandService {
 
     public void saveTemplate(CreateTemplateReqDTO createTemplateReqDTO) {
         templateDomainService.saveTemplate(createTemplateReqDTO);
+    }
+
+    public void updateTemplate(Long templateId, UpdateTemplateReqDTO updateTemplateReqDTO) {
+        templateDomainService.updateTemplate(templateId, updateTemplateReqDTO);
     }
 }
