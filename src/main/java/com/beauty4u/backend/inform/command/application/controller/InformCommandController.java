c7cf23fef7989b7fe86e63e5273228d3dde138cb
seolbin01 <se01bin@suwon.ac.kr>
2024-12-08 16:26:02 +0900

    feat: 공지사항 수정 controller 구현 #62

--- src/main/java/com/beauty4u/backend/inform/command/application/controller/InformCommandController.java
+++ src/main/java/com/beauty4u/backend/inform/command/application/controller/InformCommandController.java
@@ -4,17 +4,14 @@ import com.beauty4u.backend.common.response.ApiResponse;
 import com.beauty4u.backend.common.response.ResponseUtil;
 import com.beauty4u.backend.common.success.SuccessCode;
 import com.beauty4u.backend.common.util.CustomUserUtil;
-import com.beauty4u.backend.inform.command.application.dto.CreateInformReqDTO;
+import com.beauty4u.backend.inform.command.application.dto.InformReqDTO;
 import com.beauty4u.backend.inform.command.application.service.InformCommandService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,12 +24,23 @@ public class InformCommandController {
     @Operation(summary = "공지사항 등록", description = "관리자가 공지사항을 등록한다.")
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> saveInform(
-            @RequestBody @Valid CreateInformReqDTO createInformReqDTO) {
+            @RequestBody @Valid InformReqDTO informReqDTO) {
 
         String loginUserCode = CustomUserUtil.getCurrentUserCode();
 
-        informCommandService.saveInform(loginUserCode, createInformReqDTO);
+        informCommandService.saveInform(loginUserCode, informReqDTO);
 
         return ResponseUtil.successResponse(SuccessCode.INFORM_SAVE_SUCCESS);
+    }
+
+    @Operation(summary = "공지사항 수정", description = "관리자가 공지사항을 수정한다.")
+    @PutMapping("/{informId}")
+    public ResponseEntity<ApiResponse<Void>> updateInform(
+            @PathVariable Long informId,
+            @RequestBody @Valid InformReqDTO informReqDTO) {
+
+        informCommandService.updateInform(informId, informReqDTO);
+
+        return ResponseUtil.successResponse(SuccessCode.INFORM_UPDATE_SUCCESS);
     }
 }
