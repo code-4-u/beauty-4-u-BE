854dfa6ef64274197c2cc321eb812e9d3ad8e457
SaeRyung <sr1094@naver.com>
2024-12-09 02:18:50 +0900

    chore: 템플릿 날짜 타입 변경

--- src/main/java/com/beauty4u/backend/template/command/domain/aggregate/Template.java
+++ src/main/java/com/beauty4u/backend/template/command/domain/aggregate/Template.java
@@ -1,19 +1,18 @@
-package com.beauty4u.backend.basesystem.command.domain.aggregate;
+package com.beauty4u.backend.template.command.domain.aggregate;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.Setter;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "template")
 public class Template {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "template_id", nullable = false)
     private Long id;
 
@@ -29,9 +28,9 @@ public class Template {
 
     @NotNull
     @Column(name = "template_created_date", nullable = false)
-    private Instant templateCreatedDate;
+    private LocalDateTime templateCreatedDate;
 
     @Column(name = "template_updated_date")
-    private Instant templateUpdatedDate;
+    private LocalDateTime templateUpdatedDate;
 
 }
