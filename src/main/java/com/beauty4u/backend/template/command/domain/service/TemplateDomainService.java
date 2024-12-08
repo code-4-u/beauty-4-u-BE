b6fc4855612722b95670bac9d54112c47313e8ce
SaeRyung <sr1094@naver.com>
2024-12-09 03:43:50 +0900

    feat: TemplateDomainService 템플릿 수정 코드 추가

--- src/main/java/com/beauty4u/backend/template/command/domain/service/TemplateDomainService.java
+++ src/main/java/com/beauty4u/backend/template/command/domain/service/TemplateDomainService.java
@@ -1,12 +1,17 @@
 package com.beauty4u.backend.template.command.domain.service;
 
+import com.beauty4u.backend.common.exception.CustomException;
+import com.beauty4u.backend.common.exception.ErrorCode;
 import com.beauty4u.backend.template.command.application.dto.CreateTemplateReqDTO;
+import com.beauty4u.backend.template.command.application.dto.UpdateTemplateReqDTO;
 import com.beauty4u.backend.template.command.domain.aggregate.Template;
 import com.beauty4u.backend.template.command.domain.repository.TemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +24,18 @@ public class TemplateDomainService {
     public void saveTemplate(CreateTemplateReqDTO createTemplateReqDTO) {
         Template template = modelMapper.map(createTemplateReqDTO, Template.class);
         templateRepository.save(template);
+    }
+
+    @Transactional
+    public void updateTemplate(Long templateId, UpdateTemplateReqDTO updateTemplateReqDTO) {
+
+        String templateTitle = updateTemplateReqDTO.getTemplateName();
+        String templateContent = updateTemplateReqDTO.getTemplateContent();
+        LocalDateTime templateUpdateDate = updateTemplateReqDTO.getTemplateUpdatedDate();
+
+        Template template = templateRepository.findById(templateId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_TEMPLATE));
+
+        template.updateTemplate(templateTitle, templateContent, templateUpdateDate);
+
     }
 }
