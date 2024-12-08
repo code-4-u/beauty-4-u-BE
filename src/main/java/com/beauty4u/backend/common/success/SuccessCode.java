0b806f6aeb7cc2ccea5193dba7f300601d2958d6
seolbin01 <se01bin@suwon.ac.kr>
2024-12-08 22:54:34 +0900

    feat: 에러코드 및 성공코드 추가 #74

--- src/main/java/com/beauty4u/backend/common/success/SuccessCode.java
+++ src/main/java/com/beauty4u/backend/common/success/SuccessCode.java
@@ -24,7 +24,8 @@ public enum SuccessCode {
     INFORM_DELETE_SUCCESS(HttpStatus.OK, "공지사항 삭제 성공"),
 
     // 문의 (inquiry)
-    INQUIRY_SAVE_SUCCESS(HttpStatus.CREATED, "QnA 등록 성공");
+    INQUIRY_SAVE_SUCCESS(HttpStatus.CREATED, "QnA 등록 성공"),
+    INQUIRY_UPDATE_SUCCESS(HttpStatus.OK, "QnA 수정 성공");
 
     private final HttpStatus httpStatus;
     private final String message;
