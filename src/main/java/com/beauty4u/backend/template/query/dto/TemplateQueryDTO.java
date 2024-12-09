f4a708ac89781d5c3711c6a45b0accfc53a8f889
seolbin01 <se01bin@suwon.ac.kr>
2024-12-09 12:38:02 +0900

    refactor: dto 수정 #92

--- src/main/java/com/beauty4u/backend/template/query/dto/TemplateQueryDTO.java
+++ src/main/java/com/beauty4u/backend/template/query/dto/TemplateQueryDTO.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TemplateQueryDTO {
+
     private Long templateId;
     private String templateName;
     private String templateContent;
-    private Instant templateCreatedDate;
+    private LocalDateTime templateCreatedDate;
 }
