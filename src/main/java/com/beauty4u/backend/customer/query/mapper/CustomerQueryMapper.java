5567701cb14d108a71151bd620e3c8d15e3b08c9
leebackcoding <lcy2825@gmail.com>
2024-12-09 17:50:23 +0900

    refactor: 고객 조회 페이징 처리를 위한 메서드 추가

--- src/main/java/com/beauty4u/backend/customer/query/mapper/CustomerQueryMapper.java
+++ src/main/java/com/beauty4u/backend/customer/query/mapper/CustomerQueryMapper.java
@@ -1,9 +1,9 @@
 package com.beauty4u.backend.customer.query.mapper;
 
 import com.beauty4u.backend.customer.query.dto.CustomerDetailResDTO;
-import com.beauty4u.backend.customer.query.dto.CustomerListResDTO;
+import com.beauty4u.backend.customer.query.dto.CustomerListDTO;
 import com.beauty4u.backend.customer.query.dto.CustomerListStatsDTO;
-import com.beauty4u.backend.customer.query.dto.CustomerOrderInfoListResDTO;
+import com.beauty4u.backend.customer.query.dto.CustomerOrderInfoListDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -13,7 +13,7 @@ import java.util.List;
 @Mapper
 public interface CustomerQueryMapper {
 
-    List<CustomerListResDTO> findCustomerList(
+    List<CustomerListDTO> findCustomerList(
             @Param("customerCode") String customerCode,
             @Param("customerName") String customerName,
             @Param("customerGrade") String customerGrade,
@@ -27,7 +27,7 @@ public interface CustomerQueryMapper {
 
     CustomerDetailResDTO findCustomerDetail(@Param("customerCode") String customerCode);
 
-    List<CustomerOrderInfoListResDTO> findCustomerOrderInfoList(
+    List<CustomerOrderInfoListDTO> findCustomerOrderInfoList(
             @Param("customerCode") String customerCode,
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
@@ -40,10 +40,34 @@ public interface CustomerQueryMapper {
             @Param("order") String order,
             @Param("offset") Long offset,
             @Param("count") Long count
-            );
+    );
 
     CustomerListStatsDTO findCustomerListStats(
             @Param("now") LocalDateTime now,
             @Param("oneMonthAgo") LocalDateTime oneMonthAgo
+    );
+
+    Long findCustomerOrderInfoTotalCount(
+            @Param("customerCode") String customerCode,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            @Param("orderId") Long orderId,
+            @Param("goodsName") String goodsName,
+            @Param("minPrice") Long minPrice,
+            @Param("maxPrice") Long maxPrice,
+            @Param("orderStatus") String orderStatus,
+            @Param("sort") String sort,
+            @Param("order") String order
+    );
+
+    Long findCustomerListTotalCount(
+            @Param("customerCode") String customerCode,
+            @Param("customerName") String customerName,
+            @Param("customerGrade") String customerGrade,
+            @Param("customerGender") String customerGender,
+            @Param("startAge") Integer startAge,
+            @Param("endAge") Integer endAge,
+            @Param("sort") String sort,
+            @Param("order") String order
     );
 }
