91a0e957598b6580bfd6fa76bf62c16d2d8ce044
aosskfdlrla <gmltns885@gmail.com>
2024-12-10 14:07:39 +0900

    feat: AnalysisController 생성 #112

---
+++ src/main/java/com/beauty4u/backend/analysis/query/controller/AnalysisController.java
@@ -0,0 +1,11 @@
+package com.beauty4u.backend.analysis.query.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/analysis")
+public class AnalysisController {
+
+
+}
