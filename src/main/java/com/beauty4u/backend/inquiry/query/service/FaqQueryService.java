523b33fb576d9559e560344a8d32d244aca0ceb2
seolbin01 <se01bin@suwon.ac.kr>
2024-12-10 15:52:34 +0900

    feat: FAQ 목록 조회 service 메서드 구현 #115

---
+++ src/main/java/com/beauty4u/backend/inquiry/query/service/FaqQueryService.java
@@ -0,0 +1,24 @@
+package com.beauty4u.backend.inquiry.query.service;
+
+import com.beauty4u.backend.inquiry.query.dto.FaqListResDTO;
+import com.beauty4u.backend.inquiry.query.mapper.FaqQueryMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FaqQueryService {
+
+    private final FaqQueryMapper faqQueryMapper;
+
+    @Transactional(readOnly = true)
+    public List<FaqListResDTO> findFaqList(Long page, Long count) {
+
+        long offset = (page - 1) * count;
+
+        return faqQueryMapper.findFaqList(offset, count);
+    }
+}
