3b41a9e0cb5f96dd58f761e3e2c3c23a70ea83dd
SaeRyung <sr1094@naver.com>
2024-12-11 01:24:26 +0900

    feat: BaseSearchDocument 엘라스틱서치 document 고유 id 엔티티

---
+++ src/main/java/com/beauty4u/backend/elasticsearch/BaseSearchDocument.java
@@ -0,0 +1,13 @@
+package com.beauty4u.backend.elasticsearch;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class BaseSearchDocument {
+    @Id
+    protected String id;
+    // 검색 문서 기본 틀
+}
