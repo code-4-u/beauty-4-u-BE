1945b262a966731409c89e38fe449d4d91e87a5c
SaeRyung <sr1094@naver.com>
2024-12-10 09:08:29 +0900

    feat: MarketingSetting 엔티티 구현

---
+++ src/main/java/com/beauty4u/backend/marketing/command/domain/aggregate/MarketingSetting.java
@@ -0,0 +1,43 @@
+package com.beauty4u.backend.marketing.command.domain.aggregate;
+
+import com.beauty4u.backend.analysis.command.domain.aggregate.Analysis;
+import com.beauty4u.backend.analysis.command.domain.aggregate.AnalysisKind;
+import com.beauty4u.backend.common.aggregate.SettingChannelType;
+import com.beauty4u.backend.template.command.domain.aggregate.Template;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "marketing_setting")
+public class MarketingSetting {
+    @Id
+    @Column(name = "marketing_setting_id")
+    private Long marketingSettingId;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "analysis_id", nullable = false)
+    private Analysis analysisId;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "template_id", nullable = false)
+    private Template templateId;
+
+    @NotNull
+    @Column(name = "marketing_noti_interval", nullable = false)
+    private Integer marketingNotiInterval;
+
+    @NotNull
+    @Column(name = "analysis_kind", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AnalysisKind analysisKind;
+
+    @NotNull
+    @Column(name = "marketing_setting_channel")
+    @Enumerated(EnumType.STRING)
+    private SettingChannelType marketingSettingChannel;
+
+}
