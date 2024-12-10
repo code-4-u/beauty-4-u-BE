d574587adf0a7a07cf620f244a2d52f172ccf868
SaeRyung <sr1094@naver.com>
2024-12-11 01:28:51 +0900

    chore: GoodsDocument 상품검색 Id값 삭제

--- src/main/java/com/beauty4u/backend/goods/query/elasticsearch/document/GoodsDocument.java
+++ src/main/java/com/beauty4u/backend/goods/query/elasticsearch/document/GoodsDocument.java
@@ -1,30 +1,28 @@
 package com.beauty4u.backend.goods.query.elasticsearch.document;
 
+import com.beauty4u.backend.elasticsearch.BaseSearchDocument;
 import com.beauty4u.backend.goods.query.dto.GoodsQueryDTO;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-@Document(indexName = "goods")
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class GoodsDocument {
-    @Id
-    @Field(name = "goods_code", type = FieldType.Text)
-    private String goodsCode;
+@Document(indexName = "goods")
+public class GoodsDocument extends BaseSearchDocument {
 
     @Field(name = "goods_name", type = FieldType.Text)
     private String goodsName;
 
     public static GoodsDocument from(GoodsQueryDTO goodsQueryDTO) {
-        return GoodsDocument.builder()
-                .goodsCode(goodsQueryDTO.getGoodsCode())
+        GoodsDocument goodsDocument = GoodsDocument.builder()
                 .goodsName(goodsQueryDTO.getGoodsName())
                 .build();
+        goodsDocument.setId(goodsQueryDTO.getGoodsCode());
+        return goodsDocument;
     }
 }
