3e43cda47632ce7f09d98fb24ec26c5ad9262e94
aosskfdlrla <gmltns885@gmail.com>
2024-12-11 17:14:00 +0900

    feat: 고객 비율 분석, 연령별 구매 비율 추가 #112

--- src/main/java/com/beauty4u/backend/analysis/query/mapper/AnalysisQueryMapper.java
+++ src/main/java/com/beauty4u/backend/analysis/query/mapper/AnalysisQueryMapper.java
@@ -1,8 +1,22 @@
 package com.beauty4u.backend.analysis.query.mapper;
 
+import com.beauty4u.backend.analysis.query.dto.response.AnalysisAgeGroupRadioResDTO;
+import com.beauty4u.backend.analysis.query.dto.response.AnalysisPurchasesByAgeResDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDateTime;
 
 @Mapper
 public interface AnalysisQueryMapper {
+
+    /* 고객 비율 분석 (연령별 성별 비율 분석) */
+    AnalysisAgeGroupRadioResDTO selectAnalysisAgeGroupRadio(@Param("startDate") LocalDateTime startDate,
+                                                            @Param("endDate") LocalDateTime endDate);
+
+    /* 연령별 구매 비율 구하기 (원형 그래프) */
+    AnalysisPurchasesByAgeResDTO selectAnalysisPurchasesByAge(@Param("startDate") LocalDateTime startDate,
+                                                              @Param("endDate") LocalDateTime endDate);
+
 
 }
