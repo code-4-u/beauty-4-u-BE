0941df3aa5e21d48b1c4ba979a285b54b59518f2
leebackcoding <lcy2825@gmail.com>
2024-12-12 09:16:18 +0900

    refactor: 알림 조회 service 수정

--- src/main/java/com/beauty4u/backend/inform/query/service/InformQueryService.java
+++ src/main/java/com/beauty4u/backend/inform/query/service/InformQueryService.java
@@ -1,12 +1,15 @@
 package com.beauty4u.backend.inform.query.service;
 
 import com.beauty4u.backend.inform.query.dto.InformDetailResDTO;
+import com.beauty4u.backend.inform.query.dto.InformFilterDTO;
+import com.beauty4u.backend.inform.query.dto.InformListDTO;
 import com.beauty4u.backend.inform.query.dto.InformListResDTO;
 import com.beauty4u.backend.inform.query.mapper.InformQueryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,11 +19,47 @@ public class InformQueryService {
     private final InformQueryMapper informQueryMapper;
 
     @Transactional(readOnly = true)
-    public List<InformListResDTO> findInformList(Long page, Long count) {
+    public InformListResDTO findInformList(InformFilterDTO informFilterDTO) {
 
-        long offset = (page - 1) * count;
+        long offset = (informFilterDTO.getPage() - 1) * informFilterDTO.getCount();
 
-        return informQueryMapper.findInformList(offset, count);
+        LocalDateTime startDateTime = null;
+        LocalDateTime endDateTime = null; // 종료일의 23:59:59
+
+        if (informFilterDTO.getStartDate() != null) {
+            startDateTime = informFilterDTO.getStartDate().atStartOfDay();
+        }
+
+        if (informFilterDTO.getEndDate() != null) {
+            endDateTime = informFilterDTO.getEndDate().atStartOfDay();
+        }
+
+        List<InformListDTO> informListDTO = informQueryMapper.findInformList(
+                informFilterDTO.getInformTitle(),
+                informFilterDTO.getPublishStatus(),
+                startDateTime,
+                endDateTime,
+                informFilterDTO.getSort(),
+                informFilterDTO.getOrder(),
+                offset,
+                informFilterDTO.getCount());
+
+        InformListResDTO informListResDTO = new InformListResDTO(
+
+        );
+        informListResDTO.setInformList(informListDTO);
+
+        Long totalCount = informQueryMapper.findInformListTotalCount(
+                informFilterDTO.getInformTitle(),
+                informFilterDTO.getPublishStatus(),
+                startDateTime,
+                endDateTime,
+                informFilterDTO.getSort(),
+                informFilterDTO.getOrder()
+        );
+        informListResDTO.setTotalCount(totalCount);
+
+        return informListResDTO;
     }
 
     @Transactional(readOnly = true)
