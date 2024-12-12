d0d4786d06c7cf0d6903524015179e3feb620572
aosskfdlrla <gmltns885@gmail.com>
2024-12-12 17:06:18 +0900

    feat: Test 코드 추가 #112

---
+++ src/test/java/com/beauty4u/backend/analysis/query/service/AnalysisQueryServiceTest.java
@@ -0,0 +1,64 @@
+package com.beauty4u.backend.analysis.query.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AnalysisQueryServiceTest {
+
+
+
+    @Test
+    void selectAnalysisAgeGroupRadio() {
+    }
+
+    @Test
+    void selectAnalysisPurchasesByAge() {
+    }
+
+    @Test
+    void selectAnalysisPurchasesByBrand() {
+    }
+
+    @Test
+    void selectAnalysisPurchasesByBrandProduct() {
+    }
+
+    @Test
+    void selectAnalysisGradeGroupRadio() {
+    }
+
+    @Test
+    void selectAnalysisGradeByBrandProduct() {
+    }
+
+    @Test
+    void testSelectAnalysisGradeByBrandProduct() {
+    }
+
+    @Test
+    void selectAnalysisAgeSalesRadio() {
+    }
+
+    @Test
+    void selectAnalysisAgeSalesByBrand() {
+    }
+
+    @Test
+    void selectAnalysisAgeSalesByBrandProduct() {
+    }
+
+    @Test
+    void selectAnalysisGradeSalesRadio() {
+    }
+
+    @Test
+    void selectAnalysisGradeSalesByBrand() {
+    }
+
+    @Test
+    void selectAnalysisGradeSalesByBrandProduct() {
+    }
+}
