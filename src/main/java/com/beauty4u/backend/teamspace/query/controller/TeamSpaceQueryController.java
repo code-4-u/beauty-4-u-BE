4c2c32aa5221da7b4cd8c64e83c7ba68744c08aa
신민철 <dbeoalscjf98@naver.com>
2024-12-12 12:43:08 +0900

    "feat: 팀스페이스 사용자 정보 조회 #110 SSP-72"

--- src/main/java/com/beauty4u/backend/teamspace/query/controller/TeamSpaceQueryController.java
+++ src/main/java/com/beauty4u/backend/teamspace/query/controller/TeamSpaceQueryController.java
@@ -1,6 +1,6 @@
 package com.beauty4u.backend.teamspace.query.controller;
 
-import com.beauty4u.backend.teamspace.query.dto.teamspace.TeamSpaceResponse;
+import com.beauty4u.backend.teamspace.query.dto.teamspace.TeamSpaceUserInfoDto;
 import com.beauty4u.backend.teamspace.query.service.TeamSpaceQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/vi/teamspace")
 @RequiredArgsConstructor
@@ -18,10 +20,10 @@ public class TeamSpaceQueryController {
 
     private final TeamSpaceQueryService teamSpaceQueryService;
 
-    @GetMapping("/dept")
-    @Operation(summary = "팀스페이스 조회", description = "팀스페이스 정보를 조회한다.")
-    public TeamSpaceResponse getTeamspaceByDeptCode(@RequestParam String deptCode) {
-        // 서비스에서 DTO를 생성 및 반환
-        return teamSpaceQueryService.getTeamSpaceByDeptCode(deptCode);
+    @Operation(summary = "팀스페이스 내의 채팅 참여자 정보 조회", description = "부서 코드에 맞는 팀스페이스 참여자의 정보를 조회합니다.")
+    @GetMapping("/users")
+    public List<TeamSpaceUserInfoDto> getTeamSpaceUsersByDeptCode(
+            @RequestParam("deptCode") String deptCode) {
+        return teamSpaceQueryService.findAllTeamSpaceUser(deptCode);
     }
 }
