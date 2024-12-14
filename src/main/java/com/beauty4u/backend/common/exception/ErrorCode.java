65fbc66fddff9eaffa7b4b1ba99753ef2a624590
seolbin01 <se01bin@suwon.ac.kr>
2024-12-14 17:07:01 +0900

    feat: 에러코드 및 성공코드 추가 #146

--- src/main/java/com/beauty4u/backend/common/exception/ErrorCode.java
+++ src/main/java/com/beauty4u/backend/common/exception/ErrorCode.java
@@ -41,7 +41,10 @@ public enum ErrorCode {
     // 알림 (noti)
     NOT_SAVED_NOTI(HttpStatus.CONFLICT, "알림 등록 실패"),
     NOT_REQUEST_NOTI(HttpStatus.CONFLICT, "알림 전송 실패"),
-    NOT_FOUND_NOTI(HttpStatus.NOT_FOUND, "알림 조회 실패");
+    NOT_FOUND_NOTI(HttpStatus.NOT_FOUND, "알림 조회 실패"),
+
+    // 일정 (schedule)
+    NOT_FOUND_SCHEDULE(HttpStatus.NOT_FOUND, "일정 조회 실패");
 
     private final HttpStatus httpStatus;
     private final String message;
