d8ef54191cf6b7e66431efb058c835f2b78bdb0b
SaeRyung <sr1094@naver.com>
2024-12-15 17:28:08 +0900

    feat: ReviewQueryMapper 리뷰 기간별 조회 기능 추가

--- src/main/java/com/beauty4u/backend/goods/query/mapper/ReviewQueryMapper.java
+++ src/main/java/com/beauty4u/backend/goods/query/mapper/ReviewQueryMapper.java
@@ -3,12 +3,19 @@ package com.beauty4u.backend.goods.query.mapper;
 import com.beauty4u.backend.goods.query.dto.ReviewQueryDTO;
 import com.beauty4u.backend.goods.query.dto.ReviewSortDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Mapper
 public interface ReviewQueryMapper {
+    // 리뷰 전체 조회
     List<ReviewQueryDTO> findAllReview();
 
+    // 리뷰 정렬
     List<ReviewQueryDTO> findAllReviewSort(ReviewSortDTO reviewSortDTO);
+
+    // 리뷰 기간별 조회
+    List<ReviewQueryDTO> findAllReviewByDate(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
