0b28c7676d9e438419769b871931cd2961cea747
SaeRyung <sr1094@naver.com>
2024-12-16 22:56:05 +0900

    chore: 한글분석 매핑 추가

--- src/main/java/com/beauty4u/backend/goods/query/elasticsearch/document/ReviewDocument.java
+++ src/main/java/com/beauty4u/backend/goods/query/elasticsearch/document/ReviewDocument.java
@@ -3,9 +3,7 @@ package com.beauty4u.backend.goods.query.elasticsearch.document;
 import com.beauty4u.backend.elasticsearch.BaseSearchDocument;
 import com.beauty4u.backend.goods.query.dto.ReviewQueryDTO;
 import lombok.*;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.*;
 
 @Getter
 @Setter
@@ -13,6 +11,8 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(indexName = "reviews") // reviews 인덱스(=DB table 개념)에 데이터 저장
+@Setting(settingPath = "/elasticsearch/settings/settings.json")
+@Mapping(mappingPath = "/elasticsearch/mappings/mappings.json")
 public class ReviewDocument extends BaseSearchDocument {
 
     @Field(name = "review_content", type = FieldType.Text)
