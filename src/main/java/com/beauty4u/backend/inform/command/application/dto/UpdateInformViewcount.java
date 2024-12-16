bdea7facc1014564f33f37813f210b2a84b3a03b
leebackcoding <lcy2825@gmail.com>
2024-12-16 15:26:30 +0900

    refactor: 조회수 증가 구현

---
+++ src/main/java/com/beauty4u/backend/inform/command/application/dto/UpdateInformViewcount.java
@@ -0,0 +1,15 @@
+package com.beauty4u.backend.inform.command.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateInformViewcount {
+
+    private Long informViewcount;
+}
