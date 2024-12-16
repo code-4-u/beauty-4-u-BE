5ea7a6867914911fe636db44b2665b7f90fd7944
leebackcoding <lcy2825@gmail.com>
2024-12-16 15:24:09 +0900

    refactor: 공지사항 service 추가

--- src/main/java/com/beauty4u/backend/inform/command/application/service/InformService.java
+++ src/main/java/com/beauty4u/backend/inform/command/application/service/InformService.java
@@ -1,6 +1,9 @@
 package com.beauty4u.backend.inform.command.application.service;
 
+import com.beauty4u.backend.basesystem.command.domain.service.FileDomainService;
+import com.beauty4u.backend.common.util.S3ImageUtil;
 import com.beauty4u.backend.inform.command.application.dto.InformReqDTO;
+import com.beauty4u.backend.inform.command.application.dto.UpdateInformViewcount;
 import com.beauty4u.backend.inform.command.domain.service.InformDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class InformService {
 
     private final InformDomainService informDomainService;
+    private final FileDomainService fileDomainService;
+    private final S3ImageUtil s3ImageUtil;
 
     @Transactional
-    public void saveInform(String loginUserCode, InformReqDTO informReqDTO) {
+    public Long saveInform(String loginUserCode, InformReqDTO informReqDTO) {
 
-        informDomainService.saveInform(loginUserCode, informReqDTO);
+        return informDomainService.saveInform(loginUserCode, informReqDTO);
     }
 
     @Transactional
@@ -28,5 +33,11 @@ public class InformService {
     public void deleteInform(Long informId) {
 
         informDomainService.deleteInform(informId);
+    }
+
+    @Transactional
+    public void updateInformViewCount(Long informId, UpdateInformViewcount informViewcount) {
+
+        informDomainService.updateInformViewcount(informId, informViewcount);
     }
 }
