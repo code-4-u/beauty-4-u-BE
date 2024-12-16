fcd5d16c5984fcbb4569f4acc69c9dd81ae076cc
leebackcoding <lcy2825@gmail.com>
2024-12-17 03:55:29 +0900

    feat: 프로모션 목록 조회 service 메서드 구현

--- src/main/java/com/beauty4u/backend/promotion/query/service/PromotionQueryService.java
+++ src/main/java/com/beauty4u/backend/promotion/query/service/PromotionQueryService.java
@@ -2,10 +2,15 @@ package com.beauty4u.backend.promotion.query.service;
 
 import com.beauty4u.backend.common.exception.CustomException;
 import com.beauty4u.backend.common.exception.ErrorCode;
+import com.beauty4u.backend.promotion.query.dto.FindPromotionListReqDTO;
 import com.beauty4u.backend.promotion.query.dto.PromotionDetailResDTO;
 import com.beauty4u.backend.promotion.query.mapper.PromotionMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +28,42 @@ public class PromotionQueryService {
         }
 
         return findPromotionDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PromotionDetailResDTO> findPromotionList(FindPromotionListReqDTO findPromotionListReqDTO) {
+
+        Long offset = (findPromotionListReqDTO.getPage() - 1) * findPromotionListReqDTO.getCount();
+
+        System.out.println("offset:" + offset);
+        System.out.println("count: " + findPromotionListReqDTO.getCount());
+
+        LocalDateTime startDateTime = null;
+        LocalDateTime endDateTime = null; // 종료일의 23:59:59
+
+        System.out.println(findPromotionListReqDTO);
+
+        if (findPromotionListReqDTO.getPromotionStartDate() != null) {
+            startDateTime = findPromotionListReqDTO.getPromotionStartDate().atStartOfDay();
+        }
+
+        if (findPromotionListReqDTO.getPromotionEndDate() != null) {
+            endDateTime = findPromotionListReqDTO.getPromotionEndDate().atTime(23, 59, 59);
+        }
+
+        List<PromotionDetailResDTO> promotionList = promotionMapper.findPromotionList(
+                findPromotionListReqDTO.getPromotionTitle(),
+                startDateTime,
+                endDateTime,
+                findPromotionListReqDTO.getPromotionStatus(),
+                findPromotionListReqDTO.getSort(),
+                findPromotionListReqDTO.getOrder(),
+                offset,
+                findPromotionListReqDTO.getCount()
+        );
+
+        System.out.println(promotionList.size());
+
+        return promotionList;
     }
 }
