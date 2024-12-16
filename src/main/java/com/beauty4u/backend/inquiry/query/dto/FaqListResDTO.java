527265c94019734738926e08e5e30d60044654d7
seolbin01 <se01bin@suwon.ac.kr>
2024-12-16 15:15:07 +0900

    refactor: FAQ 목록 조회 리팩토링 #150

--- src/main/java/com/beauty4u/backend/inquiry/query/dto/FaqListResDTO.java
+++ src/main/java/com/beauty4u/backend/inquiry/query/dto/FaqListResDTO.java
@@ -3,12 +3,12 @@ package com.beauty4u.backend.inquiry.query.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class FaqListResDTO {
 
-    private String faqId;
-    private String userCode;
-    private String faqTitle;
-    private String faqViewcount;
+    private Long totalCount;
+    private List<FaqListDTO> faqList;
 }
--- src/main/java/com/beauty4u/backend/inquiry/query/mapper/FaqQueryMapper.java
+++ src/main/java/com/beauty4u/backend/inquiry/query/mapper/FaqQueryMapper.java
@@ -1,7 +1,7 @@
 package com.beauty4u.backend.inquiry.query.mapper;
 
 import com.beauty4u.backend.inquiry.query.dto.FaqDetailResDTO;
-import com.beauty4u.backend.inquiry.query.dto.FaqListResDTO;
+import com.beauty4u.backend.inquiry.query.dto.FaqListDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,9 +10,13 @@ import java.util.List;
 @Mapper
 public interface FaqQueryMapper {
 
-    List<FaqListResDTO> findFaqList(
+    List<FaqListDTO> findFaqList(
+            @Param("faqTitle") String faqTitle,
             @Param("offset") Long offset,
             @Param("count") Long count);
 
     FaqDetailResDTO findFaqDetail(@Param("faqId") Long faqId);
+
+    Long findFaqListTotalCount(
+            @Param("faqTitle") String faqTitle);
 }
