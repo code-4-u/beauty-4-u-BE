e0d974ea026b75bc4fc4e1d93de148741e82cd77
SaeRyung <sr1094@naver.com>
2024-12-17 14:31:18 +0900

    feat: MarketingSetting cud 구현

--- src/main/java/com/beauty4u/backend/marketing/command/application/service/MarketingSettingService.java
+++ src/main/java/com/beauty4u/backend/marketing/command/application/service/MarketingSettingService.java
@@ -1,5 +1,6 @@
 package com.beauty4u.backend.marketing.command.application.service;
 
+import com.beauty4u.backend.marketing.command.application.dto.MarketSettingUpdateDTO;
 import com.beauty4u.backend.marketing.command.application.dto.MarketingSettingReqDTO;
 import com.beauty4u.backend.marketing.command.domain.service.MarketingSettingDomainService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,21 @@ public class MarketingSettingService {
 
     private final MarketingSettingDomainService marketingSettingDomainService;
 
+    // 마케팅 알림 등록
     public void saveMarketingSetting(MarketingSettingReqDTO marketingSettingReqDTO) {
 
         marketingSettingDomainService.saveMarketingSetting(marketingSettingReqDTO);
+    }
+
+    // 마케팅 알림 수정
+    public void updateMarketingSetting(Long marketingSettingId, MarketSettingUpdateDTO marketSettingUpdateDTO) {
+
+        marketingSettingDomainService.updateMarketingSetting(marketingSettingId, marketSettingUpdateDTO);
+    }
+
+    // 마케팅 알림 삭제
+    public void deleteMarketingSetting(Long marketingSettingId) {
+
+        marketingSettingDomainService.deleteMarketSetting(marketingSettingId);
     }
 }
