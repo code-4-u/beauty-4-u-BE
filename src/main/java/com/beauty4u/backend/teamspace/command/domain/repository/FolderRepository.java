dc2f7202949b6cd493adedb61dd173bfbb348237
leebackcoding <lcy2825@gmail.com>
2024-12-17 15:35:12 +0900

    feat: 폴더 repository 중복 체크 메서드 추가

--- src/main/java/com/beauty4u/backend/teamspace/command/domain/repository/FolderRepository.java
+++ src/main/java/com/beauty4u/backend/teamspace/command/domain/repository/FolderRepository.java
@@ -1,10 +1,23 @@
 package com.beauty4u.backend.teamspace.command.domain.repository;
 
+import com.beauty4u.backend.common.aggregate.StatusType;
 import com.beauty4u.backend.teamspace.command.domain.aggregate.Folder;
+import com.beauty4u.backend.teamspace.command.domain.aggregate.Teamspace;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
-    Folder findByFolderName(String name);
+
+    boolean existsByFolderNameAndTeamspaceIdAndPublishStatusAndTopFolderIdIsNull(
+            @Size(max = 50) @NotNull String folderName,
+            @NotNull Teamspace teamspaceId,
+            StatusType publishStatus);
+
+    boolean existsByFolderNameAndPublishStatusAndTopFolderId(
+            @Size(max = 50) @NotNull String folderName,
+            StatusType publishStatus,
+            Folder topFolderId);
 }
