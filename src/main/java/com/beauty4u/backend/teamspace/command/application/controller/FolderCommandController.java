6eedc3c48dc37f99c629c38ec66f41a8499768b1
leebackcoding <lcy2825@gmail.com>
2024-12-17 11:29:28 +0900

    feat: 폴더 컨트롤러 endPoint 수정

--- src/main/java/com/beauty4u/backend/teamspace/command/application/controller/FolderCommandController.java
+++ src/main/java/com/beauty4u/backend/teamspace/command/application/controller/FolderCommandController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/teamspace/folder")
+@RequestMapping("/api/v1/folder")
 @RequiredArgsConstructor
 @Tag(name = "TeamSpaceFolder", description = "팀스페이스 폴더 관련 API")
 public class FolderCommandController {
