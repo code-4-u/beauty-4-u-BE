3143c69150ca02e48f68f46e3b3b362befa109c7
leebackcoding <lcy2825@gmail.com>
2024-12-17 11:24:54 +0900

    feat: 폴더 생성 service 구현

--- src/main/java/com/beauty4u/backend/teamspace/command/application/service/FolderService.java
+++ src/main/java/com/beauty4u/backend/teamspace/command/application/service/FolderService.java
@@ -1,42 +1,46 @@
 package com.beauty4u.backend.teamspace.command.application.service;
 
-import com.beauty4u.backend.teamspace.command.application.dto.folder.CreateFolderReqDto;
+import com.beauty4u.backend.teamspace.command.application.dto.folder.saveFolderDTO;
+import com.beauty4u.backend.teamspace.command.application.dto.teamspace.FindTeamspaceDTO;
 import com.beauty4u.backend.teamspace.command.domain.aggregate.Folder;
 import com.beauty4u.backend.teamspace.command.domain.aggregate.Teamspace;
-import com.beauty4u.backend.teamspace.command.domain.repository.FolderRepository;
 import com.beauty4u.backend.teamspace.command.domain.repository.TeamSpaceRepository;
+import com.beauty4u.backend.teamspace.command.domain.service.FolderDomainService;
+import com.beauty4u.backend.teamspace.command.domain.service.TeamspaceDomainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.elasticsearch.ResourceNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.time.ZonedDateTime;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class FolderService {
 
-    private final TeamSpaceRepository teamSpaceRepository;
-    private final FolderRepository folderRepository;
+    private final TeamspaceDomainService teamspaceDomainService;
+    private final FolderDomainService folderDomainService;
+    private final ModelMapper modelMapper;
 
+    @Transactional
+    public void saveFolder(saveFolderDTO saveFolderDTO) {
 
-    public void createFolder(Long teamspaceId, CreateFolderReqDto request) {
-        Teamspace teamspace = teamSpaceRepository.findById(teamspaceId)
-                .orElseThrow(() -> new ResourceNotFoundException("Teamspace not found"));
+        FindTeamspaceDTO findTeamspaceDTO = teamspaceDomainService.findTeamspaceById(saveFolderDTO.getTeamspaceId());
+
+        Teamspace teamspace = modelMapper.map(findTeamspaceDTO, Teamspace.class);
+
         // 부모 폴더 조회
-        Folder topFolder = request.getTopFolderId() != null ?
-                folderRepository.findById(request.getTopFolderId()).orElse(null) : null;
+        Folder topFolder = saveFolderDTO.getTopFolderId() != null ?
+                modelMapper.map(folderDomainService.findById(saveFolderDTO.getTopFolderId()), Folder.class) : null;
 
         // Folder 객체 생성
         Folder folder = Folder.builder()
                 .teamspaceId(teamspace)                 // 팀스페이스 설정
                 .topFolderId(topFolder)           // 부모 폴더 설정
-                .folderName(request.getFolderName())  // 폴더 이름 설정
-                .folderCreatedDate(ZonedDateTime.now())       // 생성일 설정
+                .folderName(saveFolderDTO.getFolderName())  // 폴더 이름 설정
                 .build();
 
         // 폴더 저장
-        folderRepository.save(folder);
+        folderDomainService.saveFolder(folder);
     }
 }
