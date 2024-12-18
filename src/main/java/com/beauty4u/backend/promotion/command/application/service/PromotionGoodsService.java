1292cf5fd55fbc55ae95441490e9f763fc316f81
leebackcoding <lcy2825@gmail.com>
2024-12-18 23:51:19 +0900

    feat: 프로모션 상품 삭제 service 구현

--- src/main/java/com/beauty4u/backend/promotion/command/application/service/PromotionGoodsService.java
+++ src/main/java/com/beauty4u/backend/promotion/command/application/service/PromotionGoodsService.java
@@ -1,5 +1,6 @@
 package com.beauty4u.backend.promotion.command.application.service;
 
+import com.beauty4u.backend.promotion.command.application.dto.DeletePromotionGoodsReqDTO;
 import com.beauty4u.backend.promotion.command.application.dto.SavePromotionGoodsReqDTO;
 import com.beauty4u.backend.promotion.command.domain.service.PromotionGoodsDomainService;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +17,11 @@ public class PromotionGoodsService {
     public void savePromotionGoodsList(SavePromotionGoodsReqDTO savePromotionGoodsReqDTO) {
 
         promotionGoodsDomainService.savePromotionGoodsList(savePromotionGoodsReqDTO);
+    }
+
+    @Transactional
+    public void deletePromotionGoodsList(DeletePromotionGoodsReqDTO deletePromotionGoodsReqDTO) {
+
+        promotionGoodsDomainService.deletePromotionGoodsList(deletePromotionGoodsReqDTO);
     }
 }
