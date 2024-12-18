9695158891adf13b386b9349600e851b00b46d64
leebackcoding <lcy2825@gmail.com>
2024-12-18 23:51:35 +0900

    feat: 프로모션 상품 errorCode, successCode 추가

--- src/main/java/com/beauty4u/backend/common/success/SuccessCode.java
+++ src/main/java/com/beauty4u/backend/common/success/SuccessCode.java
@@ -128,6 +128,7 @@ public enum SuccessCode {
 
     // 프로모션 적용 상품
     PROMOTION_GOODS_LIST_SAVE_SUCCESS(HttpStatus.OK, "프로모션 적용 상품 저장 성공"),
+    PROMOTION_GOODS_LIST_DELETE_SUCCESS(HttpStatus.OK, "프로모션 적용 상품 삭제 실패"),
 
     // 폴더 (folder)
     FOLDER_SAVE_SUCCESS(HttpStatus.OK, "폴더 생성 성공"),
