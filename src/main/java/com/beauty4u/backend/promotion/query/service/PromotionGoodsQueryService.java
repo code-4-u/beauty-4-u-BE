01348af7fe57e3cc90071140021b728c68412055
leebackcoding <lcy2825@gmail.com>
2024-12-19 01:02:20 +0900

    fix: 프로모션 상품 목록 조회 service 구현

--- src/main/java/com/beauty4u/backend/promotion/query/service/PromotionGoodsQueryService.java
+++ src/main/java/com/beauty4u/backend/promotion/query/service/PromotionGoodsQueryService.java
@@ -1,5 +1,7 @@
 package com.beauty4u.backend.promotion.query.service;
 
+import com.beauty4u.backend.common.exception.CustomException;
+import com.beauty4u.backend.common.exception.ErrorCode;
 import com.beauty4u.backend.promotion.query.dto.FindPromotionGoodsListFilterDTO;
 import com.beauty4u.backend.promotion.query.dto.FindPromotionGoodsListResDTO;
 import com.beauty4u.backend.promotion.query.mapper.PromotionGoodsQueryMapper;
@@ -21,13 +23,21 @@ public class PromotionGoodsQueryService {
 
         Long offset = (findPromotionGoodsListFilterDTO.getPage() - 1) * findPromotionGoodsListFilterDTO.getCount();
 
-        return promotionGoodsQueryMapper.findPromotionGoodsList(
-                promotionId,
-                findPromotionGoodsListFilterDTO.getGoodsName(),
-                findPromotionGoodsListFilterDTO.getSort(),
-                findPromotionGoodsListFilterDTO.getOrder(),
-                offset,
-                findPromotionGoodsListFilterDTO.getCount()
-        );
+        List<FindPromotionGoodsListResDTO> promotionGoodsList = null;
+
+        try {
+            promotionGoodsList = promotionGoodsQueryMapper.findPromotionGoodsList(
+                    promotionId,
+                    findPromotionGoodsListFilterDTO.getGoodsName(),
+                    findPromotionGoodsListFilterDTO.getSort(),
+                    findPromotionGoodsListFilterDTO.getOrder(),
+                    offset,
+                    findPromotionGoodsListFilterDTO.getCount()
+            );
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.PROMOTION_GOODS_LIST_NOT_FOUND);
+        }
+
+        return promotionGoodsList;
     }
 }
