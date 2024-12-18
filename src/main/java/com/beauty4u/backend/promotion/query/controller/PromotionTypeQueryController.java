4b7ac1364efe6a1ea1677bf960c67c4b92122330
leebackcoding <lcy2825@gmail.com>
2024-12-18 20:45:07 +0900

    feat: 프로모션 종류 상세 조회 controller 구현

--- src/main/java/com/beauty4u/backend/promotion/query/controller/PromotionTypeQueryController.java
+++ src/main/java/com/beauty4u/backend/promotion/query/controller/PromotionTypeQueryController.java
@@ -3,6 +3,7 @@ package com.beauty4u.backend.promotion.query.controller;
 import com.beauty4u.backend.common.response.ApiResponse;
 import com.beauty4u.backend.common.response.ResponseUtil;
 import com.beauty4u.backend.common.success.SuccessCode;
+import com.beauty4u.backend.promotion.query.dto.FindPromotionTypeDetailDTO;
 import com.beauty4u.backend.promotion.query.dto.FindPromotionTypeListDTO;
 import com.beauty4u.backend.promotion.query.dto.PromotionTypeFilterDTO;
 import com.beauty4u.backend.promotion.query.service.PromotionTypeQueryService;
@@ -10,10 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +23,7 @@ public class PromotionTypeQueryController {
 
     private final PromotionTypeQueryService promotionTypeQueryService;
 
-    @Operation(summary = "프로모션 목록 조회", description = "프로모션 목록을 조회한다.")
+    @Operation(summary = "프로모션 종류 목록 조회", description = "프로모션 종류 목록을 조회한다.")
     @GetMapping
     public ResponseEntity<ApiResponse<List<FindPromotionTypeListDTO>>> findPromotionTypeList(
             @RequestParam PromotionTypeFilterDTO promotionTypeFilterDTO
@@ -35,4 +33,16 @@ public class PromotionTypeQueryController {
 
         return ResponseUtil.successResponse(SuccessCode.PROMOTIONTYPE_LIST_FIND_SUCCESS, findPromotionTypeListDTOS);
     }
+
+    @Operation(summary = "프로모션 종류 상세 조회", description = "프로모션 종류를 상세 조회한다.")
+    @GetMapping("/{promotionTypeId}")
+    public ResponseEntity<ApiResponse<FindPromotionTypeDetailDTO>> findPromotionTypeDetail(
+            @PathVariable("promotionTypeId") Long promotionTypeId
+    ) {
+
+        FindPromotionTypeDetailDTO promotionTypeDetail = promotionTypeQueryService.findPromotionTypeDetail(promotionTypeId);
+
+        return ResponseUtil.successResponse(SuccessCode.PROMOTIONTYPE_DETAIL_FIND_SUCCESS, promotionTypeDetail);
+    }
+
 }
