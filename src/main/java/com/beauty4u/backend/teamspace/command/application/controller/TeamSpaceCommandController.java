8286683604f7ce7420b6ee0ddfef69647a3354f9
신민철 <dbeoalscjf98@naver.com>
2024-12-18 18:21:24 +0900

    "feat: 부서별 팀스페이스 생성 #192 SSP-74"

--- src/main/java/com/beauty4u/backend/teamspace/command/application/controller/TeamSpaceCommandController.java
+++ src/main/java/com/beauty4u/backend/teamspace/command/application/controller/TeamSpaceCommandController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/teamspace")
 @RequiredArgsConstructor
@@ -24,4 +26,10 @@ public class TeamSpaceCommandController {
         return ResponseEntity.status(HttpStatus.CREATED).build();  // 본문 없이 상태 코드만 반환
     }
 
+    @PostMapping("/initialize")
+    @Operation(summary = "부서별 팀스페이스 생성", description = "부서별 초기 팀스페이스를 생성한다.")
+    public ResponseEntity<Void> initializeCreateTeamSpace() {
+        List<Teamspace> teamspaces = teamSpaceService.createAndAssignTeamSpaces();
+        return ResponseEntity.status(HttpStatus.CREATED).build();  // 본문 없이 상태 코드만 반환
+    }
 }
--- src/main/java/com/beauty4u/backend/teamspace/command/domain/repository/TeamSpaceRepository.java
+++ src/main/java/com/beauty4u/backend/teamspace/command/domain/repository/TeamSpaceRepository.java
@@ -11,4 +11,7 @@ public interface TeamSpaceRepository extends JpaRepository<Teamspace, Long> {
 
     // 특정 부서코드 ID를 기준으로 팀스페이스 조회
     Optional<Teamspace> findTeamspaceByDeptCode(String deptCode);
+
+    // 해당 부서가 존재하는지 여부 확인
+    boolean existsByDeptCode(String deptCode);
 }
