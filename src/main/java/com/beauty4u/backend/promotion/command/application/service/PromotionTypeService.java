7bff3abe2760b1136c29a2daa59112bddcc5bc61
leebackcoding <lcy2825@gmail.com>
2024-12-18 17:55:02 +0900

    feat: 프로모션 종류 수정 service 구현

--- src/main/java/com/beauty4u/backend/promotion/command/application/service/PromotionTypeService.java
+++ src/main/java/com/beauty4u/backend/promotion/command/application/service/PromotionTypeService.java
@@ -1,9 +1,11 @@
 package com.beauty4u.backend.promotion.command.application.service;
 
 import com.beauty4u.backend.promotion.command.application.dto.SavePromotionTypeDTO;
+import com.beauty4u.backend.promotion.command.application.dto.UpdatePromotionTypeDTO;
 import com.beauty4u.backend.promotion.command.domain.service.PromotionTypeDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +13,15 @@ public class PromotionTypeService {
 
     private final PromotionTypeDomainService promotionTypeDomainService;
 
+    @Transactional
     public void savePromotionType(SavePromotionTypeDTO savePromotionTypeDTO) {
 
         promotionTypeDomainService.savePromotionType(savePromotionTypeDTO);
+    }
+
+    @Transactional
+    public void updatePromotionType(Long id, UpdatePromotionTypeDTO updatePromotionTypeDTO) {
+
+        promotionTypeDomainService.updatePromotionType(id, updatePromotionTypeDTO);
     }
 }
