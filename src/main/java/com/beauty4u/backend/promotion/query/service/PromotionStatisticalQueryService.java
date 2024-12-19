485182ed34168a11de65b811b6328d9e41a389f5
aosskfdlrla <gmltns885@gmail.com>
2024-12-19 16:20:55 +0900

    feat: Service 구현 #218

--- src/main/java/com/beauty4u/backend/promotion/query/service/PromotionStatisticalQueryService.java
+++ src/main/java/com/beauty4u/backend/promotion/query/service/PromotionStatisticalQueryService.java
@@ -1,10 +1,32 @@
 package com.beauty4u.backend.promotion.query.service;
 
+import com.beauty4u.backend.promotion.query.dto.FindPromotionByComparisonSalesResDTO;
+import com.beauty4u.backend.promotion.query.dto.FindPromotionByTypeResDTO;
+import com.beauty4u.backend.promotion.query.dto.FindPromotionByYearSalesResDTO;
+import com.beauty4u.backend.promotion.query.mapper.PromotionStatisticalMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PromotionStatisticalQueryService {
 
+    private final PromotionStatisticalMapper promotionStatisticalMapper;
+
+    /* 프로모션 타입별 조회 ASC 조회 */
+    List<FindPromotionByTypeResDTO> findTypeByPromotion(){
+        return promotionStatisticalMapper.findTypeByPromotion();
+    }
+
+    /* 프로모션 종류에 따른 년도별 그래프 조회 */
+    List<FindPromotionByYearSalesResDTO> findPromotionByYearSales(List<Integer> promotionIds){
+        return promotionStatisticalMapper.findPromotionByYearSales(promotionIds);
+    }
+
+    /* 프로모션 기간 상품 매출 랭킹 비교 */
+    List<FindPromotionByComparisonSalesResDTO> findPromotionByComparisonSales(Integer previousPromotionId, Integer afterPromotionId){
+        return promotionStatisticalMapper.findPromotionByComparisonSales(previousPromotionId, afterPromotionId);
+    }
 }
