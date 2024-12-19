8975fc059a34025de52b94979a051a3eda64ebda
aosskfdlrla <gmltns885@gmail.com>
2024-12-19 16:17:08 +0900

    feat: Controller Service 추가 #218

---
+++ src/main/java/com/beauty4u/backend/promotion/query/controller/PromotionStatisticalQueryController.java
@@ -0,0 +1,11 @@
+package com.beauty4u.backend.promotion.query.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/* 프로모션 통계 컨트로롤러 */
+@RestController
+@RequiredArgsConstructor
+public class PromotionStatisticalQueryController {
+
+}
---
+++ src/main/java/com/beauty4u/backend/promotion/query/service/PromotionStatisticalQueryService.java
@@ -0,0 +1,10 @@
+package com.beauty4u.backend.promotion.query.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PromotionStatisticalQueryService {
+
+}
