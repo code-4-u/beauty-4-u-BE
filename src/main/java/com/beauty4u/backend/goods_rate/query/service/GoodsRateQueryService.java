0a409610d8d3f3ad4125897c585b8256a81058b0
SaeRyung <sr1094@naver.com>
2024-12-19 22:23:59 +0900

    feat: GoodsRateQueryService 날짜별 상품 매출 상승,하락률 구현

--- src/main/java/com/beauty4u/backend/goods_rate/query/service/GoodsRateQueryService.java
+++ src/main/java/com/beauty4u/backend/goods_rate/query/service/GoodsRateQueryService.java
@@ -1,11 +1,16 @@
 package com.beauty4u.backend.goods_rate.query.service;
 
+import com.beauty4u.backend.common.aggregate.PeriodType;
 import com.beauty4u.backend.goods_rate.query.dto.GoodsRateQueryDTO;
+import com.beauty4u.backend.goods_rate.query.dto.GoodsRateResDTO;
 import com.beauty4u.backend.goods_rate.query.mapper.GoodsRateQueryMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -13,9 +18,16 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class GoodsRateQueryService {
 
+    private static final Logger log = LoggerFactory.getLogger(GoodsRateQueryService.class);
     private final GoodsRateQueryMapper goodsRateQueryMapper;
 
-    public List<GoodsRateQueryDTO> findGoodsRateIncrease() {
-        return goodsRateQueryMapper.findGoodsRateIncrease();
+    public List<GoodsRateQueryDTO> findGoodsRate(LocalDateTime startDate, LocalDateTime endDate, PeriodType periodType) {
+
+//        return goodsRateQueryMapper.findGoodsRate(startDate, endDate, periodType.name());
+
+        log.info("조회 시작 - startDate: {}, endDate: {}, periodType: {}", startDate, endDate, periodType);
+        List<GoodsRateQueryDTO> result = goodsRateQueryMapper.findGoodsRate(startDate, endDate, periodType.name());
+        log.info("조회 결과 크기: {}", result.size());
+        return result;
     }
 }
