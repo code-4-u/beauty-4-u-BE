e17bf153abbbf4259e2528fb72f44e7e3a69a4c9
SaeRyung <sr1094@naver.com>
2024-12-19 22:23:26 +0900

    feat: GoodsRateQueryController 날짜별 상품 매출 상승,하락률 구현

--- src/main/java/com/beauty4u/backend/goods_rate/query/controller/GoodsRateQueryController.java
+++ src/main/java/com/beauty4u/backend/goods_rate/query/controller/GoodsRateQueryController.java
@@ -1,10 +1,11 @@
 package com.beauty4u.backend.goods_rate.query.controller;
 
+import com.beauty4u.backend.common.aggregate.PeriodType;
 import com.beauty4u.backend.common.response.ApiResponse;
 import com.beauty4u.backend.common.response.ResponseUtil;
 import com.beauty4u.backend.common.success.SuccessCode;
-import com.beauty4u.backend.goods.query.dto.GoodsQueryDTO;
 import com.beauty4u.backend.goods_rate.query.dto.GoodsRateQueryDTO;
+import com.beauty4u.backend.goods_rate.query.dto.GoodsRateResDTO;
 import com.beauty4u.backend.goods_rate.query.service.GoodsRateQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,25 +13,49 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/goodsRate")
-@Tag(name="GoodsRate", description = "상품 매출 조회 API")
+@Tag(name = "GoodsRate", description = "상품 매출 조회 API")
 public class GoodsRateQueryController {
 
     private final GoodsRateQueryService goodsRateQueryService;
 
-    @Operation(summary = "매출 상승률 조회", description = "메인 화면 매출 상승률을 조회한다.")
+    @Operation(summary = "일일 매출률 조회", description = "메인 화면 일일 매출 상승,하강률을 조회한다.")
     @GetMapping("/list")
-    public ResponseEntity<ApiResponse<List<GoodsRateQueryDTO>>> findGoodsRateIncrease() {
+    public ResponseEntity<ApiResponse<GoodsRateResDTO>> findGoodsRate(@RequestParam PeriodType periodType) {
+        LocalDateTime endDate = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59);
+        LocalDateTime startDate = calculateStartDate(periodType, endDate).withHour(0).withMinute(0).withSecond(0);
 
-        List<GoodsRateQueryDTO> goodsRateList = goodsRateQueryService.findGoodsRateIncrease();
+        List<GoodsRateQueryDTO> goodsRateList = goodsRateQueryService.findGoodsRate(startDate, endDate, periodType);
 
-        return ResponseUtil.successResponse(SuccessCode.GOODS_FIND_LIST_INCREASE_RATE_SUCCESS, goodsRateList);
+        GoodsRateResDTO goodsRateResDTO = GoodsRateResDTO.builder()
+                .increase(goodsRateList.stream()
+                        .filter(rate -> "INCREASE".equals(rate.getRateType()))
+                        .collect(Collectors.toList()))
+                .decrease(goodsRateList.stream()
+                        .filter(rate -> "DECREASE".equals(rate.getRateType()))
+                        .collect(Collectors.toList()))
+                .build();
+
+        return ResponseUtil.successResponse(SuccessCode.GOODS_FIND_LIST_RATE_SUCCESS, goodsRateResDTO);
     }
 
+    private LocalDateTime calculateStartDate(PeriodType periodType, LocalDateTime endDate) {
+        return switch (periodType) {
+            case DAILY -> endDate.minusDays(1);
+            case WEEKLY -> endDate.minusWeeks(1);
+            case MONTHLY -> endDate.minusMonths(1);
+            case QUARTER -> endDate.minusMonths(3);
+            case HALF -> endDate.minusMonths(6);
+            case YEARLY -> endDate.minusYears(1);
+        };
+    }
 }
