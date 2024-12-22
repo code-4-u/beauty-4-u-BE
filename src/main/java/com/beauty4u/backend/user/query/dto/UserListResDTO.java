67a7fbe5903d34ed7bd0644688e188a050456ba2
leebackcoding <lcy2825@gmail.com>
2024-12-23 01:18:00 +0900

    refactor: 목록 총 개수도 응답에 추가

--- src/main/java/com/beauty4u/backend/user/query/dto/UserListResDTO.java
+++ src/main/java/com/beauty4u/backend/user/query/dto/UserListResDTO.java
@@ -3,18 +3,12 @@ package com.beauty4u.backend.user.query.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 public class UserListResDTO {
 
-    private String userCode;
-    private String userName;
-    private String userRoleName;
-    private String deptName;
-    private String jobName;
-    private LocalDateTime createdDate;
-    private LocalDateTime userExpiredDate;
-    private String userExpiredYn;
+    List<UserListDTO> content;
+    private Long totalElements;
 }
