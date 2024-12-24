f1a83343783c22141e3fa986469d52e986e7455a
신민철 <dbeoalscjf98@naver.com>
2024-12-24 12:25:20 +0900

    "fix: 불필요한 메서드 제거 및 수정 #235 SSP-74"

--- src/main/java/com/beauty4u/backend/teamspace/query/controller/ChatRoomQueryController.java
+++ src/main/java/com/beauty4u/backend/teamspace/query/controller/ChatRoomQueryController.java
@@ -18,11 +18,6 @@ public class ChatRoomQueryController {
 
     private final ChatRoomQueryService chatRoomQueryService;
 
-    @Operation(summary = "채팅방 ID 조회", description = "부서 코드로 채팅방 ID를 조회한다.")
-    @GetMapping("")
-    public Long getMyTeamSpaceId(@RequestParam("deptCode") String deptCode) {
-        return chatRoomQueryService.getMyTeamSpaceIdByDeptCode(deptCode);
-    }
 
     @Operation(summary = "채팅방 내의 채팅 참여자 정보 조회", description = "부서 코드에 맞는 채팅방 참여자의 정보를 조회합니다.")
     @GetMapping("/users")
@@ -31,11 +26,6 @@ public class ChatRoomQueryController {
         return chatRoomQueryService.findAllChatRoomUser(deptCode);
     }
 
-    @Operation(summary = "채팅방 ID로 부서 코드 조회", description = "채팅방 ID에 맞는 부서 코드를 조회합니다.")
-    @GetMapping("/{chatRoomId}/dept")
-    public String getDeptCodeByTeamspaceId(@PathVariable String chatRoomId) {
-        return chatRoomQueryService.getDeptCodeByTeamspaceId(chatRoomId);
-    }
 
     // 팀스페이스 통합 컨트롤러
     @Operation(summary = "채팅방 채팅 상세조회", description = "채팅방 채팅방 참여자, 채팅 목록을 조회합니다.")
