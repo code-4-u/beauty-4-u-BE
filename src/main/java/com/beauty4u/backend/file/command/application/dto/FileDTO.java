cad9af509ebdc44b72ad9f5817a9f8f3564d0bb5
신민철 <dbeoalscjf98@naver.com>
2024-12-24 12:10:04 +0900

    "feat: 채팅 메세지 파일 첨부를 위한 DTO 수정 #235 SSP-74"

--- src/main/java/com/beauty4u/backend/file/command/application/dto/FileDTO.java
+++ src/main/java/com/beauty4u/backend/file/command/application/dto/FileDTO.java
@@ -2,6 +2,7 @@ package com.beauty4u.backend.file.command.application.dto;
 
 import com.beauty4u.backend.inform.command.domain.aggregate.Inform;
 import com.beauty4u.backend.inquiry.command.domain.aggregate.Inquiry;
+import com.beauty4u.backend.teamspace.command.domain.aggregate.ChatMessage;
 import com.beauty4u.backend.teamspace.command.domain.aggregate.TeamBoard;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +18,7 @@ public class FileDTO {
     private Long fileId;
     private String fileUrl;
     private TeamBoard teamBoard;
+    private ChatMessage message;
     private Inform inform;
     private Inquiry inquiry;
 }
