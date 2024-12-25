bdc7785b2f6a5350b2cf5b84a24be1fbce0baede
신민철 <dbeoalscjf98@naver.com>
2024-12-25 22:55:21 +0900

    "feat: 채팅 조회 Controller 추가  #280 SSP-77"

--- src/main/java/com/beauty4u/backend/teamspace/query/controller/ChatRoomQueryController.java
+++ src/main/java/com/beauty4u/backend/teamspace/query/controller/ChatRoomQueryController.java
@@ -1,5 +1,6 @@
 package com.beauty4u.backend.teamspace.query.controller;
 
+import com.beauty4u.backend.common.util.CustomUserUtil;
 import com.beauty4u.backend.teamspace.query.dto.chatroom.ChatRoomDetailsDto;
 import com.beauty4u.backend.teamspace.query.dto.chatroom.ChatRoomUserInfoDto;
 import com.beauty4u.backend.teamspace.query.service.ChatRoomQueryService;
@@ -20,17 +21,21 @@ public class ChatRoomQueryController {
 
 
     @Operation(summary = "채팅방 내의 채팅 참여자 정보 조회", description = "부서 코드에 맞는 채팅방 참여자의 정보를 조회합니다.")
-    @GetMapping("/users")
-    public List<ChatRoomUserInfoDto> getTeamSpaceUsersByDeptCode(
-            @RequestParam("deptCode") String deptCode) {
-        return chatRoomQueryService.findAllChatRoomUser(deptCode);
+    @GetMapping("/{chatRoomId}/users")
+    public List<ChatRoomUserInfoDto> getChatMemberByChatRoomId(
+            @PathVariable Long chatRoomId) {
+
+        String loginUserCode = CustomUserUtil.getCurrentUserCode();
+        return chatRoomQueryService.findAllChatRoomUser(chatRoomId, loginUserCode);
     }
 
 
     // 팀스페이스 통합 컨트롤러
     @Operation(summary = "채팅방 채팅 상세조회", description = "채팅방 채팅방 참여자, 채팅 목록을 조회합니다.")
     @GetMapping("/{chatRoomId}/details")
-    public ChatRoomDetailsDto getTeamSpaceDetails(@PathVariable Long chatRoomId, @RequestParam String deptCode) {
-        return chatRoomQueryService.getChatRoomDetails(chatRoomId, deptCode);
+    public ChatRoomDetailsDto getChatDetails(@PathVariable Long chatRoomId) {
+
+        String loginUserCode = CustomUserUtil.getCurrentUserCode();
+        return chatRoomQueryService.getChatRoomDetails(chatRoomId, loginUserCode);
     }
 }
