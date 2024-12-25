aac05a57d858edf82452bf4ca67fe04bf0131c24
신민철 <dbeoalscjf98@naver.com>
2024-12-25 22:54:06 +0900

    "feat: ChatRoom 생성 컨트롤러 추가  #280 SSP-77"

--- src/main/java/com/beauty4u/backend/teamspace/command/application/controller/ChatRoomCommandController.java
+++ src/main/java/com/beauty4u/backend/teamspace/command/application/controller/ChatRoomCommandController.java
@@ -3,8 +3,10 @@ package com.beauty4u.backend.teamspace.command.application.controller;
 import com.beauty4u.backend.common.response.ApiResponse;
 import com.beauty4u.backend.common.response.ResponseUtil;
 import com.beauty4u.backend.common.success.SuccessCode;
+    import com.beauty4u.backend.common.util.CustomUserUtil;
+import com.beauty4u.backend.teamspace.command.application.dto.chatroom.ChatRoomResponseDto;
+import com.beauty4u.backend.teamspace.command.application.dto.chatroom.InviteUsersToChatRoomReqDto;
 import com.beauty4u.backend.teamspace.command.application.service.ChatRoomService;
-import com.beauty4u.backend.teamspace.command.domain.aggregate.ChatRoom;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,9 +23,12 @@ public class ChatRoomCommandController {
 
     @PostMapping("/create")
     @Operation(summary = "채팅방 생성", description = "채팅방을 생성한다.")
-    public ResponseEntity<ApiResponse<Void>> createTeamSpace(@RequestParam String deptCode) {
-        ChatRoom chatRoom =  chatRoomService.createTeamspace(deptCode);
-        return ResponseUtil.successResponse(SuccessCode.TEAMSPACE_SAVE_SUCCESS);
-    }
+    public ResponseEntity<ApiResponse<ChatRoomResponseDto>> createTeamSpace(@RequestBody InviteUsersToChatRoomReqDto reqDto) {
 
+        String loginUserCode = CustomUserUtil.getCurrentUserCode();
+
+        ChatRoomResponseDto chatRoomResponseDto = chatRoomService.createChatRoom(loginUserCode, reqDto);
+
+        return ResponseUtil.successResponse(SuccessCode.CHATROOM_SAVE_SUCCESS, chatRoomResponseDto);
+    }
 }
