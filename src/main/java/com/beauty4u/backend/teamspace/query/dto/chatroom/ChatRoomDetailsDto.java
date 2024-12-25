d61f2228bb6d46e861d61f20c1b08ae3c8c5d9f3
신민철 <dbeoalscjf98@naver.com>
2024-12-25 22:59:11 +0900

    "feat: 채팅방 정보 조회 DTO 수정 #280 SSP-77"

--- src/main/java/com/beauty4u/backend/teamspace/query/dto/chatroom/ChatRoomDetailsDto.java
+++ src/main/java/com/beauty4u/backend/teamspace/query/dto/chatroom/ChatRoomDetailsDto.java
@@ -13,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChatRoomDetailsDto {
-    private String deptName;
-    private List<ChatRoomUserInfoDto> participants;
-    private List<ChatMessageResDto> messages;
+    private Long chatRoomId;                  // 채팅방 ID
+    private List<ChatRoomUserInfoDto> participants; // 채팅방 참여자 정보
+    private List<ChatMessageResDto> messages;       // 채팅 메시지 리스트
 }
