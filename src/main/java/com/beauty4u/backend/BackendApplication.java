45366faa88a7ad1abbc8e517b084373cdf704dbd
SaeRyung <sr1094@naver.com>
2025-01-01 03:26:16 +0900

    chore: 엘라스틱 빈 중첩 에러 위한 예외처리

--- src/main/java/com/beauty4u/backend/BackendApplication.java
+++ src/main/java/com/beauty4u/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.beauty4u.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class, ElasticsearchRestClientAutoConfiguration.class})
 @EnableMongoRepositories
 @EnableJpaAuditing
 public class BackendApplication {
