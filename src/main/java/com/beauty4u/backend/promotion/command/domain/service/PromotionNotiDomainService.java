0b559d0529c434123a11f2ae39d1780499b0559c
aosskfdlrla <gmltns885@gmail.com>
2025-01-06 15:25:14 +0900

    chore: 전송 이메일 추가 #338

--- src/main/java/com/beauty4u/backend/promotion/command/domain/service/PromotionNotiDomainService.java
+++ src/main/java/com/beauty4u/backend/promotion/command/domain/service/PromotionNotiDomainService.java
@@ -89,24 +89,24 @@ public class PromotionNotiDomainService {
     public PromotionEmailResult sendPromotionNotiMail(Long promotionId, Long analysisId) {
         List<FindPromotionNotiTargetResDTO> targets = promotionNotiQueryService.findPromotionNotiTarget(promotionId, analysisId);
 
-        String promotionTitle = targets.get(0).getPromotionTitle();
-        String promotionGoods = targets.get(0).getGoodsName();
+        String promotionTitle1 = targets.get(0).getPromotionTitle();
+        String promotionGoods1 = targets.get(0).getGoodsName();
 
         LocalDateTime sendDateTime = LocalDateTime.now();
         List<CompletableFuture<Void>> emailFutures = new ArrayList<>();
         Map<String, String> emailMap = new HashMap<>();  // 실패 추적용
 
         String content01 = "황희순 고객님~! \n"
-                         + "[" + promotionTitle + "]\n"
+                         + "[" + promotionTitle1 + "]\n"
                          + "행사 맞이하여 추천상품\n"
-                         + "[" + promotionGoods + "]\n"
+                         + "[" + promotionGoods1 + "]\n"
                          + "25% 할인된 가격!\n"
                          + "특가로 확인해보세요~!\n"
                          + "언제나 이용해주셔서 감사합니다!\n"
                          + "beauty4u 드림. \n"
                          + "본 메일은 프로모션 이메일 테스트 용입니다.";
 
-        CompletableFuture<Void> emailFuture1 = mailUtil.sendPromotionGmailAsync("gmltns885@gmail.com", "beauty4u - [2024 크리스마스 기념 윈터 빅세일] 프로모션 안내", content01)
+        CompletableFuture<Void> emailFuture1 = mailUtil.sendPromotionGmailAsync("gmltns885@gmail.com", "beauty4u - [" + promotionTitle1 + "] 프로모션 안내", content01)
                          .exceptionally(throwable -> {
                             emailMap.put("gmltns885@gmail.com", throwable.getMessage());
                             return null;
@@ -124,17 +124,20 @@ public class PromotionNotiDomainService {
         // DB 저장
         promotionNotiRepository.save(saveNoti1);
 
+        String promotionTitle2 = targets.get(1).getPromotionTitle();
+        String promotionGoods2 = targets.get(1).getGoodsName();
+
         String content02 = "신민철 고객님~! \n"
-                + "[" + promotionTitle + "]\n"
+                + "[" + promotionTitle2 + "]\n"
                 + "행사 맞이하여 추천상품\n"
-                + "[" + promotionGoods + "]\n"
+                + "[" + promotionGoods2 + "]\n"
                 + "25% 할인된 가격!\n"
                 + "특가로 확인해보세요~!\n"
                 + "언제나 이용해주셔서 감사합니다!\n"
                 + "beauty4u 드림. \n"
                 + "본 메일은 프로모션 이메일 테스트 용입니다.";
 
-        CompletableFuture<Void> emailFuture2 = mailUtil.sendPromotionGmailAsync("dbeoalscjf98@naver.com", "beauty4u - [2024 크리스마스 기념 윈터 빅세일] 프로모션 안내", content02)
+        CompletableFuture<Void> emailFuture2 = mailUtil.sendPromotionGmailAsync("dbeoalscjf98@naver.com", "beauty4u - [" + promotionTitle2 + "] 프로모션 안내", content02)
                          .exceptionally(throwable -> {
                             emailMap.put("dbeoalscjf98@naver.com", throwable.getMessage());
                             return null;
@@ -152,17 +155,20 @@ public class PromotionNotiDomainService {
         // DB 저장
         promotionNotiRepository.save(saveNoti2);
 
+        String promotionTitle3 = targets.get(2).getPromotionTitle();
+        String promotionGoods3 = targets.get(2).getGoodsName();
+
         String content03 = "안세령 고객님~! \n"
-                + "[" + promotionTitle + "]\n"
+                + "[" + promotionTitle3 + "]\n"
                 + "행사 맞이하여 추천상품\n"
-                + "[" + promotionGoods + "]\n"
+                + "[" + promotionGoods3 + "]\n"
                 + "25% 할인된 가격!\n"
                 + "특가로 확인해보세요~!\n"
                 + "언제나 이용해주셔서 감사합니다!\n"
                 + "beauty4u 드림. \n"
                 + "본 메일은 프로모션 이메일 테스트 용입니다.";
 
-        CompletableFuture<Void> emailFuture3 = mailUtil.sendPromotionGmailAsync("sr1094@naver.com", "beauty4u - [2024 크리스마스 기념 윈터 빅세일] 프로모션 안내", content03)
+        CompletableFuture<Void> emailFuture3 = mailUtil.sendPromotionGmailAsync("sr1094@naver.com", "beauty4u - [" + promotionTitle3 + "] 프로모션 안내", content03)
                          .exceptionally(throwable -> {
                             emailMap.put("sr1094@naver.com", throwable.getMessage());
                             return null;
@@ -172,7 +178,7 @@ public class PromotionNotiDomainService {
 
         SavePromotionNotiDTO save3 = new SavePromotionNotiDTO();
         save3.setCustomerCode("CU0000004");
-        save3.setPromotionNotiContent(content02);
+        save3.setPromotionNotiContent(content03);
         save3.setPromotionNotiSentDate(sendDateTime);
 
         PromotionNoti saveNoti3 = modelMapper.map(save3, PromotionNoti.class);
@@ -180,11 +186,41 @@ public class PromotionNotiDomainService {
         // DB 저장
         promotionNotiRepository.save(saveNoti3);
 
+        String promotionTitle4 = targets.get(3).getPromotionTitle();
+        String promotionGoods4 = targets.get(3).getGoodsName();
+
+        String content04 = "이창윤 고객님~! \n"
+                + "[" + promotionTitle4 + "]\n"
+                + "행사 맞이하여 추천상품\n"
+                + "[" + promotionGoods4 + "]\n"
+                + "25% 할인된 가격!\n"
+                + "특가로 확인해보세요~!\n"
+                + "언제나 이용해주셔서 감사합니다!\n"
+                + "beauty4u 드림. \n"
+                + "본 메일은 프로모션 이메일 테스트 용입니다.";
+
+        CompletableFuture<Void> emailFuture4 = mailUtil.sendPromotionGmailAsync("cloadelee@gmail.com", "beauty4u - [+ " + promotionTitle4 + "] 프로모션 안내", content04)
+                .exceptionally(throwable -> {
+                    emailMap.put("sr1094@naver.com", throwable.getMessage());
+                    return null;
+                });
+
+        emailFutures.add(emailFuture4);
+
+        SavePromotionNotiDTO save4 = new SavePromotionNotiDTO();
+        save4.setCustomerCode("CU0000005");
+        save4.setPromotionNotiContent(content04);
+        save4.setPromotionNotiSentDate(sendDateTime);
+
+        PromotionNoti saveNoti4 = modelMapper.map(save4, PromotionNoti.class);
+
+        // DB 저장
+        promotionNotiRepository.save(saveNoti4);
 
         return PromotionEmailResult.builder()
-                .totalCount(3)
+                .totalCount(4)
                 .failCount(emailMap.size())
-                .successCount(3 - emailMap.size())
+                .successCount(4 - emailMap.size())
                 .failEmails(new ArrayList<>(emailMap.keySet()))
                 .build();
     }
